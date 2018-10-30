8e9553b71b36f017fb9e76bae01bb89edd550467
olegush <43958767+olegush@users.noreply.github.com>
2018-10-30 16:17:17 +0300

    commited the task

--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,36 @@
+from datetime import datetime
+from datetime import timedelta
+import requests
+import json
 
 
-def get_trending_repositories(top_size):
-    pass
+def get_trending_repositories():
+    git_url = 'https://api.github.com/search/repositories'
+    time_week_ago = datetime.now() - timedelta(days=7)
+    day_week_ago = str(time_week_ago.strftime('%Y-%m-%d'))
+    repos_url = git_url + '?q=created:>' + day_week_ago + '&sort=stars'
+    repos_response = requests.get(repos_url)
+    return json.loads(repos_response.text)['items']
 
 
-def get_open_issues_amount(repo_owner, repo_name):
-    pass
+def get_open_issues(repo_owner, repo_name):
+    git_url = 'https://api.github.com/repos/'
+    issues_url = git_url + repo_owner + '/' + repo_name + '/issues?state=open'
+    issues_response = requests.get(issues_url)
+    return json.loads(issues_response.text)
+
 
 if __name__ == '__main__':
-    pass
+    top_size = 2
+    trending_repositories = get_trending_repositories()
+    for repo in trending_repositories[:top_size]:
+        print('\nOWNER: {} USER: {} STARS: {} OPEN ISSUES: {}'.
+              format(repo['owner']['login'],
+                     repo['name'],
+                     repo['stargazers_count'],
+                     repo['open_issues_count']
+                     ))
+        print("All open {}'s issues:".format(repo['name']))
+        open_issues = get_open_issues(repo['owner']['login'], repo['name'])
+        for issue in open_issues:
+            print('#{}: {}'.format(issue['number'], issue['html_url']))
